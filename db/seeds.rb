118196c698bd21bd3c963ba40a3a4361851f4e0b
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-07 12:08:43 +0100

    Simple form gem added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times {
+  Restaurant.create(
+    name: Faker::SiliconValley.company,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+}
